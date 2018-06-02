a05c752b7efe223060202f2428fa60cb8f9b71f4
Gabriel Zarate <gabrielzzarate@gmail.com>
2018-06-02 06:55:40 -0400

    add webpack/react configuration

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -12,11 +12,35 @@ module.exports = {
 				}
 			},
 			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: 'css-loader!sass-loader'
-				})
+				test: /\.css$/,
+				loader:
+					'style-loader!css-loader?modules=true&localIdentName=[name]__[local]__[hash:base64:5]'
+				// use: ExtractTextPlugin.extract({
+				// 	fallback: 'style-loader',
+				// 	use: ['css-loader']
+				// })
+				// {
+				// 	test: /\.css/,
+				// 	use: ExtractTextPlugin.extract(
+				// 		'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+				// 	)
+				// }
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif|ico)$/,
+				use: ['file-loader']
 			}
 		]
 	},
